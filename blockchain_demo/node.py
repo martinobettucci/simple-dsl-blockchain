886a97213305214ac7be202c70969ae2457e65b1
Martino Bettucci <martinobettucci@users.noreply.github.com>
2025-07-17 16:29:33 +0200

    Fix node signing logic and add genesis tests

--- blockchain_demo/node.py
+++ blockchain_demo/node.py
@@ -24,24 +24,46 @@ class Node:
         txs = self.mempool.pop_for_block(CFG.BLOCK_TX_CAP)
         prev_hash = self.chain[-1].hash() if self.chain else '0'*64
         height = len(self.chain)
-        block = Block.create_candidate(prev_hash, height, self.wallet['public_key'], txs, self.state, self.balances)
+        block = Block.create_candidate(
+            prev_hash,
+            height,
+            self.wallet['public_key'],
+            txs,
+            self.state,
+            self.balances,
+        )
         block.proof_of_work(CFG.DIFFICULTY_BITS)
-        # sign with all validator wallets for demo
-        validators = json.load(open(os.path.join('blockchain_demo', 'validators.json')))['validators']
-        validator_set = [v['pubkey'] for v in validators]
-        for idx, val in enumerate(validators, start=1):
-            wallet_path = os.path.join('wallets', f'validator{idx}.json')
-            if os.path.exists(wallet_path):
-                val_wallet = load_wallet(wallet_path)
-                sig = sign(val_wallet, block.hash())
-                block.add_validator_signature(val_wallet['public_key'], sig, validator_set)
+
+        validators = json.load(
+            open(os.path.join('blockchain_demo', 'validators.json'))
+        )["validators"]
+        validator_set = [v["pubkey"] for v in validators]
+        if (
+            "validator" in self.wallet.get("local_role", "")
+            and self.wallet["public_key"] in validator_set
+        ):
+            sig = sign(self.wallet, block.hash())
+            block.add_validator_signature(
+                self.wallet["public_key"], sig, validator_set
+            )
+
         try:
             block.finalize(validator_set, self.balances, CFG)
         except ValueError:
             pass
+
         self.chain.append(block)
-        self.state = block.state
-        self.balances = block.balances
-        with open(os.path.join('blockchain_demo/blocks', f"{block.hash()}.json"), 'w') as f:
+        if block.finalized:
+            self.state = block.state
+            self.balances = block.balances
+
+        target_dir = (
+            os.path.join('blockchain_demo', 'blocks')
+            if block.finalized
+            else os.path.join('blockchain_demo', 'pending')
+        )
+        os.makedirs(target_dir, exist_ok=True)
+        with open(os.path.join(target_dir, f"{block.hash()}.json"), "w") as f:
             json.dump(block.to_json(), f, indent=2)
+        return block
 
--- genesys.py
+++ genesys.py
@@ -1,6 +1,6 @@
 import os
 import json
-from blockchain_demo.wallet import generate_wallet, sign
+from blockchain_demo.wallet import generate_wallet
 from blockchain_demo.block import Block, BlockHeader
 from blockchain_demo.config import CFG
 
@@ -9,13 +9,11 @@ def main():
     os.makedirs('wallets', exist_ok=True)
     validators = []
     balances = {}
-    sigs = {}
     for idx, name in enumerate(["Val-A", "Val-B", "Val-C"], start=1):
         path = os.path.join('wallets', f'validator{idx}.json')
         wallet_data = generate_wallet(path, local_role='validator')
         validators.append({"pubkey": wallet_data["public_key"], "name": name})
         balances[wallet_data["public_key"]] = 100
-        sigs[wallet_data["public_key"]] = sign(wallet_data, 'genesis')
 
     validators_json = {"validators": validators, "quorum_percent": CFG.QUORUM_PERCENT}
     with open(os.path.join('blockchain_demo', 'validators.json'), 'w') as f:
@@ -28,7 +26,15 @@ def main():
         json.dump({"balances": balances}, f, indent=2)
 
     header = BlockHeader(prev_hash='0'*64, height=0, nonce=0, timestamp=0, miner='genesis')
-    genesis_block = Block(header=header, transactions=[], state=state, balances=balances, finalized=True, validator_signatures=sigs, signers_frozen=sorted(list(sigs.keys())))
+    genesis_block = Block(
+        header=header,
+        transactions=[],
+        state=state,
+        balances=balances,
+        finalized=True,
+        validator_signatures={},
+        signers_frozen=[],
+    )
     genesis_json = genesis_block.to_json()
     genesis_json["hash"] = '0'*64
     os.makedirs(os.path.join('blockchain_demo', 'blocks'), exist_ok=True)
